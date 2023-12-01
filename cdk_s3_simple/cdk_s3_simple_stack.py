e9f44fedf34493eeab8fb5ba9fd48f03b582275b
Nick Kirby <33106137+Kirizan@users.noreply.github.com>
2023-12-01 11:40:10 -0500

    Simple s3 bucket configuration

--- cdk_s3_simple/cdk_s3_simple_stack.py
+++ cdk_s3_simple/cdk_s3_simple_stack.py
@@ -1,7 +1,7 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_s3 as _s3,
+    RemovalPolicy
 )
 from constructs import Construct
 
@@ -10,10 +10,8 @@ class CdkS3SimpleStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkS3SimpleQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        _s3.Bucket(
+          self, "Bucket",
+          auto_delete_objects=True,
+          removal_policy=RemovalPolicy.DESTROY
+        )
