94143ad64e95934f8c698cda68d72034931a6a92
Nick Kirby <33106137+Kirizan@users.noreply.github.com>
2023-12-04 09:41:01 -0500

    Changed from CDK_DEFAULT to CDK_DEPLOY for ACCN and Region.

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 import os
-
 import aws_cdk as cdk
-
 from cdk_s3_simple.cdk_s3_simple_stack import CdkS3SimpleStack
-
-
 app = cdk.App()
 CdkS3SimpleStack(app, "CdkS3SimpleStack",
-    env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+    env=cdk.Environment(
+      account=os.getenv('CDK_DEPLOY_ACCOUNT'), 
+      region=os.getenv('CDK_DEPLOY_REGION')),
     )
 
 app.synth()
